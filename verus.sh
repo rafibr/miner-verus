7cac73475c22ee17b07f555be91ed1450a75641b
Muhammad Rafi B.R <rafi.brilliansyah@gmail.com>
2021-05-28 03:34:57 +0800

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget httpsL//github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u asdasdasdasd.test -p x --cpu 4
+
